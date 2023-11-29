b1e593244ecea99d7917b04dd6474faa4f36a49d
vrunda patel <149460401+9624420393@users.noreply.github.com>
2023-11-29 20:30:04 +0530

    Add files via upload

---
+++ average electrcity bill.cpp
@@ -0,0 +1,14 @@
+//WAP to find Average electricity bill//
+
+
+#include<stdio.h>
+main()
+{
+	int a=23,b=32,c=64;
+	float average=(a+b+c)/3; // equation to find average//
+
+	printf("the average monthly electricity bill:$%f",average);  // output to monthly bill//
+	
+	
+	return 0;
+}
