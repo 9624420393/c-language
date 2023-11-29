87372c716459be942ae763b520cf85d34a986bd7
vrunda patel <149460401+9624420393@users.noreply.github.com>
2023-11-29 13:28:09 +0530

    Add files via upload

---
+++ loops_1/hello.cpp
@@ -0,0 +1,13 @@
+//WAP to perform hello for 10 times//
+
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=1;i<=10;i++)
+	{
+		printf("(%d)hello\n",i);
+				
+	}
+	return 0;
+}
---
+++ loops_1/inputs.cpp
@@ -0,0 +1,13 @@
+//WAP to get times inputs from the users//
+
+#include<stdio.h>
+main()
+{
+	int i,num;
+	for(i=1;i<=10;i++)
+	{
+	  printf("enter the input:");
+	  scanf("%d",&num);
+	}
+	return 0;
+}
